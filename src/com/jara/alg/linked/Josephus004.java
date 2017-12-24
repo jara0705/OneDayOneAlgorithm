b624cd070d2d02ba1d902705dbb96a9ff67f776e
jara0705 <jiaqi767175348@126.com>
2017-12-24 21:28:05 +0800

    Josephus plus

--- src/com/jara/alg/linked/Josephus004.java
+++ src/com/jara/alg/linked/Josephus004.java
@@ -14,7 +14,8 @@ public class Josephus004 {
         node.next.next.next.next = new Node(5);
         node.next.next.next.next.next = new Node(6);
         node.next.next.next.next.next.next = node;
-        josephus(node, 1);
+//        josephus(node, 1);
+        josephusplus(node, 1);
     }
 
     private static void josephus(Node node, int m) {
@@ -37,6 +38,31 @@ public class Josephus004 {
         }
         System.out.println(node.value + " ");
 
+    }
+
+    private static void josephusplus(Node node, int m) {
+        if (node == null || m < 1) {
+            return;
+        }
+        Node cur = node.next;
+        int tmp = 1;
+        while (cur != node) {
+            tmp++;
+            cur = cur.next;
+        }
+        tmp = getLive(tmp, m);
+        while (--tmp != 0) {
+            node = node.next;
+        }
+        node.next = node;
+        System.out.println(node.value);
+    }
+
+    private static int getLive(int i, int m) {
+        if (i == 1) {
+            return 1;
+        }
+        return (getLive(i-1, m) + m - 1) % i - 1;
     }
 
 }
